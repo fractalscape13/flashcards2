f80cf9458e63706ec252982bad4183d43c8a39fa
Joseph Wangemann and Rachel Schieferstein <rachel@>
2020-01-14 11:53:07 -0800

    added html + jquery functions for toggling

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    $(".hidetext1").toggle();
+    $(".showtext1").toggle();
+  });
+  $("#btn2").click(function(){
+    $(".hidetext2").toggle();
+    $(".showtext2").toggle();
+  });
+
+  $("#btn3").click(function(){
+      $(".hidetext3").toggle();
+      $(".showtext3").toggle();
+  });
+
+  $("#btn4").click(function(){
+    $(".hidetext4").toggle();
+    $(".showtext4").toggle();
+  });
+  $("#btn5").click(function(){
+    $(".hidetext5").toggle();
+    $(".showtext5").toggle();
+  });
+
+  $("#btn6").click(function(){
+      $(".hidetext6").toggle();
+      $(".showtext6").toggle();
+  });
+  $("#btn7").click(function(){
+    $(".hidetext7").toggle();
+    $(".showtext7").toggle();
+  });
+  $("#btn8").click(function(){
+    $(".hidetext8").toggle();
+    $(".showtext8").toggle();
+  });
+
+  $("#btn9").click(function(){
+      $(".hidetext9").toggle();
+      $(".showtext9").toggle();
+  });
+ 
+});
